594daa5892b57601d202b325218d2793d535b9e8
Manni Moghimi <mannimoghimi@gmail.com>
2025-07-01 22:33:21 +0330

    chore: add multi worker test scenario

--- tests/main.py
+++ tests/main.py
@@ -17,7 +17,7 @@ FUNCTIONS = [
         "command": "python3.10",
         "filepath": "eigenvalue/eigenvalue.py",
         "benchmark_inputs": ["2000"],
-        "run_inputs": ["2000", "1500", "2500"],
+        "run_inputs": ["1500", "2000", "2500"],
     },
 ]
 
@@ -73,8 +73,8 @@ if __name__ == "__main__":
 
     try:
         # single worker test
-        # test_scenario(["http://localhost:3000"], 10)
-        # print(f"Testing Successful: %SINGLE WORKER%")
+        test_scenario(["http://localhost:3000"], 10)
+        print(f"Testing Successful: %SINGLE WORKER%")
 
         # multi worker test
         test_scenario(["http://localhost:3000", "http://localhost:3100"], 10)
